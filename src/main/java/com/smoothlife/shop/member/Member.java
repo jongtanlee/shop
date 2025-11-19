9aea82873f58319a904aeee8622ec6f0c68cbdb1
jtlee <jongtan.lee@gmail.com>
2025-11-19 18:58:20 +0900

    first [2025.11.19]

---
+++ src/main/java/com/smoothlife/shop/member/Member.java
@@ -0,0 +1,118 @@
+package com.smoothlife.shop.member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Schema(description = "유저 정보")
+@Data
+@Entity
+@Table(name = "\"member\"", schema = "public")
+public class Member {
+
+    @Schema(description = "유저의 UUID")
+    @Id
+    private UUID id;
+
+    @Schema(description = "유저의 이메일")
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Schema(description = "유저명")
+    @Column(name = "\"name\"", length = 20)
+    private String name;
+
+    @Schema(description = "유저의 비밀번호")
+    @Column(name = "\"password\"", nullable = false, length = 100)
+    private String password;
+
+    @Schema(description = "유저의 핸드폰번호")
+    @Column(nullable = false, length = 20, unique = true)
+    private String phone;
+
+    @Schema(description = "등록자")
+    @Column(name = "reg_id", nullable = false)
+    private UUID regId;
+
+    @Schema(description = "등록시간")
+    @Column(name = "reg_dt", nullable = false)
+    private LocalDateTime regDt;
+
+    @Schema(description = "수정자")
+    @Column(name = "modify_id", nullable = false)
+    private UUID modifyId;
+
+    @Schema(description = "수정시간")
+    @Column(name = "modify_dt", nullable = false)
+    private LocalDateTime modifyDt;
+
+    @Schema(description = "saltkey")
+    @Column(name = "saltkey", nullable = false, length = 14)
+    private String saltKey;
+
+    @Schema(description = "유저의 사용자상태")
+    @Column(name = "flag", length = 5)
+    private String flag;
+
+    public Member(UUID id,
+                  String email,
+                  String name,
+                  String password,
+                  String phone,
+                  String saltKey,
+                  String flag) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.saltKey = saltKey;
+        this.flag = flag;
+    }
+    public Member(String id,
+                  String email,
+                  String name,
+                  String password,
+                  String phone,
+                  String saltKey,
+                  String flag) {
+        this.id = UUID.fromString(id);
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.saltKey = saltKey;
+        this.flag = flag;
+    }
+    public Member() {}
+
+    @PrePersist
+    public void prePersist() {
+        if (regId == null) {
+            regId = id != null ? id : UUID.randomUUID();
+        }
+        if (modifyId == null) {
+            modifyId = regId;
+        }
+        if (regDt == null) {
+            regDt = LocalDateTime.now();
+        }
+        if (modifyDt == null) {
+            modifyDt = LocalDateTime.now();
+        }
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        modifyDt = LocalDateTime.now();
+        if (modifyId == null) {
+            modifyId = id;
+        }
+    }
+
+}
